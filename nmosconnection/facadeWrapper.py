c167fd5bc0f961623de6351cd80eddf75113b758
Simon Rankine <simon.rankine@bbc.co.uk>
2018-03-09 14:38:29 +0000

    Fix flow and schema resources to match schema properly

--- nmosconnection/facadeWrapper.py
+++ nmosconnection/facadeWrapper.py
@@ -36,7 +36,7 @@ class SimpleFacadeWrapper:
         self.deviceId = deviceId
         self.deviceData = {"id": self.deviceId, "label":"example mock device", "description": "A pretend device used as part of the NMOS IS-04/05 reference implementation.", "tags": {}, "type": "urn:x-nmos:device:generic", "senders":[], "receivers":[], "\
 controls": [], "max_api_version" : "v1.2" }
-        self.facade.addResource("device",self.deviceId,self.deviceData)
+        #self.facade.addResource("device",self.deviceId,self.deviceData)
         self.updateDevice()
 
     def updateDevice(self):
@@ -53,7 +53,7 @@ controls": [], "max_api_version" : "v1.2" }
         # Register receiver
         receiverData = {"id": receiverId, "label":"example mock receiver", "description": "A pretend receiver used as part of the NMOS IS-04/05 reference implementation.", "tags": {}, "format": "urn:x-nmos:format:video", "caps": { "media_types": ["video/raw"]}, "subscription": {"sender_id": None, "active": False}, "transport": "urn:x-nmos:transport:rtp", "interface_bindings": [ "eth0" ], "device_id": self.deviceId, "max_api_version": "v1.2"}
         self.receivers[receiverId] = receiverData
-        self.facade.addResource("receiver",receiverId,receiverData)
+        #self.facade.addResource("receiver",receiverId,receiverData)
         self.deviceData['receivers'].append(receiverId)
         self.updateReceiver(receiverId)
         self.updateDevice()
@@ -75,7 +75,7 @@ controls": [], "max_api_version" : "v1.2" }
         # Register source
         sourceData = {"id": sourceId, "label":"example mock source", "description": "A pretend source used as part of the NMOS IS-04/05 reference implementation.", "tags": {}, "format": "urn:x-nmos:format:video", "caps": {}, "parents": [], "device_id": self.deviceId, "clock_name": "clk1", "max_api_version": "v1.2"}
         self.sources[sourceId] = sourceData
-        self.facade.addResource("source",sourceId,sourceData)
+        #        self.facade.addResource("source",sourceId,sourceData)
         self.updateSource(sourceId)
         self.updateDevice()
 
@@ -91,11 +91,52 @@ controls": [], "max_api_version" : "v1.2" }
         self.sources.pop(key)
         self.updateDevice()
 
+    def makeFlowComponents(self):
+        components = [{
+            "name": "Y",
+            "width": 1920,
+            "height": 1080,
+            "bit_depth": 10
+        }, {
+            "name": "Cb",
+            "width": 960,
+            "height": 1080,
+            "bit_depth": 10
+        }, {
+            "name": "Cb",
+            "width": 960,
+            "height": 1080,
+            "bit_depth": 10
+        }
+        ]
+        return components
+
+    def makeFlowData(self, flowId, sourceId):
+        flowData = {
+            "id": flowId,
+            "label": "example mock flow",
+            "description": "A pretend flow used as part of the NMOS IS-04\
+reference implementation.",
+            "tags": {},
+            "source_id": sourceId,
+            "device_id": self.deviceId,
+            "parents": [],
+            "format": "urn:x-nmos:format:video",
+            "frame_width": 1920,
+            "frame_height": 1080,
+            "colorspace": "BT709",
+            "max_api_version": "v1.2",
+            "interlace_mode": "interlaced_tff",
+            "media_type": "video/raw"
+        }
+        return flowData
+
     def registerFlow(self, flowId, sourceId):
         # Register flow
-        flowData = {"id": flowId, "label": "example mock flow", "description": "A pretend flow used as part of the NMOS IS-04/05 reference implementation.", "tags": {}, "source_id": sourceId, "device_id": self.deviceId, "parents": [], "format": "urn:x-nmos:format:video", "frame_width": 1920, "frame_height": 1080,  "colorspace" : "BT709", "max_api_version": "v1.2"}
+        flowData = self.makeFlowData(flowId, sourceId)
+        flowData['components'] = self.makeFlowComponents()
         self.flows[flowId] = flowData
-        self.facade.addResource("flow",flowId,flowData)
+        #        self.facade.addResource("flow",flowId,flowData)
         self.updateFlow(flowId)
         self.updateDevice()
 
@@ -119,20 +160,44 @@ controls": [], "max_api_version" : "v1.2" }
         except IndexException:
             return interfaces[0]
 
+    def makeSenderData(self, senderId, flowId):
+        interface = self.getInterface()
+        senderData = {
+            "id": senderId,
+            "label": "example mock sender",
+            "description": "A pretend sender used as part of the NMOS IS-04/05\
+reference implementation.",
+            "tags": {},
+            "flow_id": flowId,
+            "transport": "urn:x-nmos:transport:rtp",
+            "device_id": self.deviceId,
+            "manifest_href": "http://localhost:8080/x-nmos/connection/v1.0/\
+single/senders/{}/transportfile/".format(senderId),
+            "interface_bindings": [interface],
+            "max_api_version": "v1.2",
+            "subscription": {
+                "receiver_id": None,
+                "active": False
+            }
+        }
+        return senderData
+
     def registerSender(self, senderId, flowId):
         # Register sender
         self.deviceData['senders'].append(senderId)
-        interface = self.getInterface()
-        senderData = {"id": senderId, "label": "example mock sender", "description": "A pretend sender used as part of the NMOS IS-04/05 reference implementation.", "tags": {}, "flow_id": flowId, "transport": "urn:x-nmos:transport:rtp", "device_id": self.deviceId, "manifest_href": "", "interface_bindings": [interface], "max_api_version": "v1.2"}
+        senderData = self.makeSenderData(senderId, flowId)
         self.senders[senderId] = senderData
-        self.facade.addResource("sender",senderId,senderData)
-        self.updateSender(senderId)
+        self.updateSenderVersion(senderId)
+        self.facade.addResource("sender", senderId, senderData)
         self.updateDevice()
+
+    def updateSenderVersion(self, senderId):
+        timeNow = ptptime.ptp_detail()
+        self.senders[senderId]['version'] = "{}:{}".format(repr(timeNow[0]), repr(timeNow[1]))
 
     def updateSender(self, senderId):
         # Push our local copy of receiver data up, and increment version number
-        timeNow = ptptime.ptp_detail()
-        self.senders[senderId]['version'] = "{}:{}".format(repr(timeNow[0]), repr(timeNow[1]))
+        self.updateSenderVersion(senderId)
         self.facade.updateResource("sender", senderId, self.senders[senderId])
 
     def delSender(self, key):
